3f0d79ce2d2b9f5514fd1abb4dd7604721e2a2be
edo <e.phoenixconsulting@gmail.com>
2022-03-14 11:58:24 +0100

    main

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 // 
 // computer choose random between paper rock scissors//
 function computerPlay() {
-    let game = ["paper","rock","scissors"]
+    let game = ["paper","rock","scissors"];
     let gameToUse = game[Math.floor(Math.random() * game.length)];
-
-    console.log(gameToUse);
+        return gameToUse;
+    
 }
 computerPlay();
 
 // player choose one of the option//
 function playRound(playerSelection, computerSelection) {
-    if(computerSelection < playerSelection){
-        return ("You Lose! Paper beats Rock");
+    
+    if(playerSelection === computerSelection){
+        return ("Tie");
+    }else if(playerSelection === 'rock'){
+            if( computerSelection === 'scissors'){
+  
+            return ("You win");
+            }else{
+            return ("You lose");
+    }
+    }else if(playerSelection === 'scissor' && computerSelection === 'paper'){
+        return ("You win");
+    }else{
+        return ("You lose");
     }
 }
 playRound();
 const playerSelection = "rock";
 const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+console.log(playerSelection)
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection))
+   
 // the game finish in one term with the winner declared// 
