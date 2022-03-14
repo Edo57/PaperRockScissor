dadacd8229bdc53ece9c41b202392e1c487fb51a
edo <e.phoenixconsulting@gmail.com>
2022-03-14 14:03:58 +0100

    main

--- script.js
+++ script.js
@@ -10,20 +10,20 @@ function computerPlay() {
 computerPlay();
 
 // player choose one of the option//
-function playRound(playerSelecTion, comPuterSelectIon) {
-     
-    if(playerSelecTion === comPuterSelectIon){
+function playRound(playerSelection, computerSelection) {
+   
+    if(playerSelection === computerSelection){
     
         return ("Tie");
-    }else if(playerSelecTion === 'rock'){
-            if( comPuterSelectIon === 'scissors'){
+    }else if(playerSelection === 'rock'){
+            if( computerSelection === 'scissors'){
             userScore += 1 ; 
             return ("You win");
             }else{
             computerScore+= 1;
             return ("You loose");
     }
-    }else if(playerSelecTion === 'scissor' && comPuterSelectIon === 'paper'){
+    }else if(playerSelection === 'scissor' && computerSelection === 'paper'){
         userScore += 1 ; 
         return ("You win");
     }else{
@@ -35,20 +35,24 @@ function playRound(playerSelecTion, comPuterSelectIon) {
 let computerScore = 0;
 let userScore = 0; 
 playRound();
-let playerSelecTion = prompt("Pick a move");
-const comPuterSelectIon = computerPlay();
-console.log(playerSelecTion);
-console.log(comPuterSelectIon);
-console.log(playRound(playerSelecTion, comPuterSelectIon));
+let playerSelection = prompt("Pick a move");
+const computerSelection = computerPlay();
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
    
 // loop for 5 round// 
 
-
-for(var i=0;i<5;i++){
+function game(){
+    for(var i=0;i<5;i++){
     
-    let playerSelection = prompt("Pick a move");
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection))
-    console.log("your score = " + userScore);
-    console.log("Computer's score = " + computerScore);
-  }
+        let playerSelection = prompt("Pick a move");
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("your score = " + userScore);
+        console.log("Computer's score = " + computerScore);
+        console.log(playerSelection);
+        console.log(computerSelection);
+      }
+}
+game();
