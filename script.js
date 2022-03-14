8710da297e0590c18e327f02200fd78deca05044
edo <e.phoenixconsulting@gmail.com>
2022-03-14 09:44:50 +0100

    main

---
+++ script.js
@@ -0,0 +1,12 @@
+// 
+// computer choose random between paper rock scissors//
+function computerPlay() {
+    const game = ["paper","rock","scissors"];
+    const gameToUse = game[Math.floor(math.random()* length)];
+
+    alert(gameToUse);
+}
+
+console.log(gameToUse);
+// player choose one of the option//
+// the game finish in one term with the winner declared// 
